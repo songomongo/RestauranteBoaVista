a027bc75bba7add9fffac254633a4244baa8f23f
songomongo <henriquegoisr@gmail.com>
2024-03-21 12:56:33 -0300

    commit

--- src/Restaurante.tsx
+++ src/Restaurante.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, ImageBackground, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 
 interface MenuItem {
@@ -16,57 +16,107 @@ interface MenuItem {
             id: '1', 
             nome: 'Carne Grelhada ao molho tuschê', 
             descripition: 'Carne grelhada ao molho tuschê, que é composto por banana, tomate, cebola e sazon', 
-            price: 'R$90.00', 
-            image: '',
+            price: 'R$168.00', 
+            image: require('./assets/images/Bifeangus.png'),
+            
         },
         {
             id: '2', 
             nome: 'Macarronada alho e oleo', 
             descripition: 'macarrão com alho e oleo cozido com bacon', 
-            price: 'R$78.00', 
-            image: '',
+            price: 'R$160.00', 
+            image: require('./assets/images/images.png'),
         },
         {
             id: '3', 
             nome: 'Frango assado com folhas de eucalipito', 
             descripition: 'frango assado no forno com pequenas folhas de eucalipito verde', 
             price: 'R$65.00', 
-            image: '',
+            image: require('./assets/images/images(2).png'),
         },
         {
             id: '4', 
             nome: 'Picanha assada de ângus', 
             descripition: 'picanha de ângus assada na lenha com sal do topo do imalaia', 
             price: 'R$120.00', 
-            image: '',
+            image: require('./assets/images/images(3).png'),
         },
         {
             id: '5', 
             nome: 'Bolinho de arroz japonês', 
             descripition: 'arroz, sal, pimenta do reino, e folhas de hortlã', 
             price: 'R$50.00', 
-            image: '',
+            image: require('./assets/images/images(4).png'),
         },
         {
             id: '6', 
             nome: 'Sopa tailandesa', 
-            descripition: '', 
+            descripition: 'sopa da tailandia com tempero', 
             price: 'R$89.00', 
-            image: '',
+            image: require('./assets/images/images(5).png'),
         },
         {
             id: '7', 
-            nome: 'Carne Grelhada ao molho da casa', 
-            descripition: 'Carne grelhada ao molho da casa, que é composto por banana, tomate, cebola e sazon', 
-            price: 'R$78.00', 
-            image: '',
+            nome: 'Lagostinha', 
+            descripition: 'lagosta ao molho madeira, com frutos do mar', 
+            price: 'R$125.00', 
+            image: require('./assets/images/images(6).png'),
         },
         {
             id: '8', 
-            nome: 'Carne Grelhada ao molho da casa', 
-            descripition: 'Carne grelhada ao molho da casa, que é composto por banana, tomate, cebola e sazon', 
-            price: 'R$78.00', 
-            image: '',
+            nome: 'Frango frito', 
+            descripition: 'frango frito com tempero da casa', 
+            price: 'R$90.00', 
+            image: require('./assets/images/images(7).png'),
+        },
+        {
+            id: '9', 
+            nome: 'Camarão com feijão preto', 
+            descripition: 'camarão ao feijão preto com tempero especial', 
+            price: 'R$235.00', 
+            image: require('./assets/images/images(8).png'),
+        },
+        {
+            id: '10', 
+            nome: 'Caldo de Peixe', 
+            descripition: 'Caldo de frutos dor mar com molho especial', 
+            price: 'R$219.00', 
+            image: require('./assets/images/images(9).png'),
+        },
+        {
+            id: '11', 
+            nome: 'Chiclte de Camarão', 
+            descripition: 'Camarão com 6 tipos de queijo diferente queijo', 
+            price: 'R$147.00', 
+            image: require('./assets/images/images(10).png'),
+        },
+        {
+            id: '12', 
+            nome: 'Bolinho de Carne Porco', 
+            descripition: 'Carne de porco levemente assada e cozida com tempero completo', 
+            price: 'R$150.00', 
+            image: require('./assets/images/frangobranco.png'),
+        },
+        {
+            id: '13', 
+            nome: 'Polvo Polvilhado', 
+            descripition: 'Polvo, tomate e macarronada com tempero especial', 
+            price: 'R$300.00', 
+            image: require('./assets/images/images1.png'),
+        },
+        {
+            id: '14', 
+            nome: 'Carne Seca com Mandioca', 
+            descripition: 'Carne seca especial, com mais de 100 anos, e mandioca do himalaia', 
+            price: 'R$129.00', 
+            image: require('./assets/images/macarronada.png'),
+        },
+        {
+            id: '15', 
+            nome: 'Ostra com Batata', 
+            descripition: 'Ostra com batata cozida', 
+            price: 'R$270.00', 
+            image: require('./assets/images/pancetapure.png'),
         },
   
 
@@ -74,26 +124,39 @@ interface MenuItem {
 
     const renderItem = ({ item }: { item: MenuItem}) => (
         <TouchableOpacity style={styles.item}>
-            <Text>{ item.nome }</Text>
+            
+            <Text style= {styles.text}>{item.descripition}</Text>
+            <Text style= {styles.text}>{ item.price}</Text>
+            <Image source={item.image} style={styles.imagem}></Image>
    
         </TouchableOpacity>
     );
 
+   const renderBar = ({ bar }: { bar: MenuItem}) => (
+        <TouchableOpacity style={styles.item}>
+            <Text style= {styles.text}>{ bar.nome }</Text>
+        </TouchableOpacity>
+   );
 
-function Restaurante(): React.JSX.Element {
+
+ function Restaurante(): React.JSX.Element {
     return (
         <View style={ styles.container }>
-            <StatusBar backgroundColor="grey" barStyle='light-content'/>
+            <ImageBackground source={require('./assets/images/cinza.png')}  style={styles.imagemB}>
+            <StatusBar backgroundColor="black" barStyle='light-content'/>
             <View style={styles.header}>
-                <Text style={styles.headerText}>Flat list</Text>
-            </View>
+               
+                    <Image source={require('./assets/images/restaurante.png')} style={styles.logoTipo}/>
+                </View>
             <FlatList
-             showsVerticalScrollIndicator={false}
-             data={dados}
-             renderItem={renderItem}
-             keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            data={dados}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            
             />
-
+             </ImageBackground>
+           
 
             <View  style={styles.footer}>
                 <TouchableOpacity>
@@ -133,19 +196,53 @@ function Restaurante(): React.JSX.Element {
     const styles = StyleSheet.create({
         container: {
             flex: 1,
+            backgroundColor: '#003840'
+
+        },
+
+        bar: {
+            backgroundColor: 'white',
+            padding: 20,
+
+
+        },
+        logoTipo: {
+            marginHorizontal:1,
+            width: 390,
+            height: 70
+        },
+
+        text: {
+            marginHorizontal: 10,
+            marginVertical: 5,
+            marginTop: 10,
+            color: 'black',
+            fontSize: 14,
+            
 
         },
         item: {
-            backgroundColor: 'cyan',
+            backgroundColor: '#757575',
             padding: 20,
-            marginVertical: 8,
-            marginHorizontal: 16
+            marginVertical: 9,
+            marginHorizontal: 30,
+            borderWidth: 3.0,
+            borderColor: 'white',
+            opacity:0.9,
+            
+
+        },
+        imagem: {
+            width: 150,
+            height: 150,
+            margin: 15,
+        
             
         },
         header: {
-            backgroundColor: 'blue',
+            backgroundColor: 'white',
             alignItems: 'center',
-            paddingVertical: 25
+                    
         },
         headerText: {
             fontSize: 20,
@@ -155,11 +252,12 @@ function Restaurante(): React.JSX.Element {
         },
         footer: {
             borderBottomWidth:0.2,
-            backgroundColor: 'white',
+            backgroundColor: '#AFAFAF',
             flexDirection: 'row',
             justifyContent: 'space-around',
             alignItems: 'center',
-            paddingVertical: 10
+
+            paddingVertical: 15
 
         },
         footerIcon: {
@@ -167,6 +265,11 @@ function Restaurante(): React.JSX.Element {
             height: 40
 
         },
+        imagemB: {
+            flex: 1,
+            justifyContent: 'center'
+        }
+
 
     })
 
